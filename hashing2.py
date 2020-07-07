e5205772bbca9604912e4344525f6e90047f1b7a
Tom Tarpey <tomtarpeydev@gmail.com>
2020-07-07 11:17:31 +0100

    augmentation day 2

--- hashing2.py
+++ hashing2.py
@@ -1,3 +1,34 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Insert (for putting a node)
+    def insert_at_head(self, node):
+        node.next = self.head
+        self.head = node
+
+    # Find (for getting a node)
+    def find(self, value):
+        cur = self.head
+
+        while cur is not None:
+            if cur.value == value:
+                return cur
+            
+            cur = cur.next
+        
+        return None
+
+    # Delete (for deleting a node)
+    # stretch
+
+
+
 # hashtableentry class 
 # key, value
 # repr
@@ -7,6 +38,8 @@ class HashTableEntry:
     def __init__(self, key, value):
         self.key = key
         self.value = value
+        # self.bob = "hello"
+        self.next = None
 
     def repr(self):
         return f"HashTableEntry({self.key}, {self.value})"
@@ -64,3 +97,31 @@ print(f'Hello value is: {v}') # Should be "Hello Value", but gives "foo value"
 # Doesn't exist!
 v = get("Frogs")
 print(v)  # "None"
+
+
+"""
+Put
+---
+1: find index
+2: search the list at the index for the provided key
+3: if the key exists > overwrite the value
+4: otherwise key does not exist
+::: make a new HashTableEntry with the key and value
+::: insert the HashTableEntry object at the head of the list
+
+Get
+---
+1: find index
+2: search list at index for a matching key
+3: return objects value or None if not found
+
+Delete
+------
+1: find index
+2: search linked list at the index for the hte object that matches the key
+3: if found:
+::: delete entry from linked list
+::: return hte value
+4: otherwise:
+::: return None
+"""
